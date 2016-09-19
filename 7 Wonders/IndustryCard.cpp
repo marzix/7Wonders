20e2677ba556f84e837101a334bfa7759531b620
Comandoraf <rafal.potoczek@gmail.com>
2016-09-19 23:04:05 +0200

    Clear

--- 7 Wonders/IndustryCard.cpp
+++
@@ -1,7 +0,0 @@
-#include "IndustryCard.h"
-
-IndustryCard::IndustryCard( eCardType type, short int epoque, Cost cost, vector<eMaterial> material, eBonusType b )
-: Card( type, epoque, cost )
-, materials( material )
-, bonus( b )
-{}
